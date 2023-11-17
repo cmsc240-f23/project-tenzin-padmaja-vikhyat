a0d8e4d81bc38b3cd5284b83af6af911b62a304d
PadmajaKarki <padmajakusum@gmail.com>
2023-11-17 14:05:41 -0500

    changes

--- Cat.h
+++ Cat.h
@@ -5,4 +5,24 @@ using namespace std;
 #ifndef CAT_H
 #define CAT_H
 
-class 
+class Cat
+{
+    public:
+        Cat(int id, string name, string breed, int age, bool availableForAdoption);
+        int getId();
+        string getRescueStory();
+        Cat Adopt();
+        string getCatInfo();
+        void convertToJson();
+        void updateFromJson();
+
+
+    private:
+        int id;
+        string name;
+        string breed;
+        int age;
+        bool availableForAdoption;
+
+};
+#endif
